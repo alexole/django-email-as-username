9256c5531b285851b110a57ecc3b9adaee0965bd
Alexander Olekhnovich <alexander.olehnovich@viaden.com>
2014-09-16 13:51:19 +0300

    Custom user model support

--- emailusernames/backends.py
+++ emailusernames/backends.py
@@ -1,5 +1,8 @@
 from django.contrib.auth.models import User
 from django.contrib.auth.backends import ModelBackend
+from django.db.models import get_model
+from django.conf import settings
+from django.core.exceptions import ImproperlyConfigured
 
 from emailusernames.utils import get_user
 
@@ -23,6 +26,14 @@ class EmailAuthBackend(ModelBackend):
 
     def get_user(self, user_id):
         try:
-            return User.objects.get(pk=user_id)
-        except User.DoesNotExist:
+            return self.user_class.objects.get(pk=user_id)
+        except self.user_class:
             return None
+
+    @property
+    def user_class(self):
+        if not hasattr(self, '_user_class'):
+            self._user_class = get_model(*settings.CUSTOM_USER_MODEL.split('.', 2))
+            if not self._user_class:
+                raise ImproperlyConfigured('Could not get custom user model')
+        return self._user_class
